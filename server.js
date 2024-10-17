72f07c7f2b1d83d97e01b53b938995d59d53f520
Mughees <mughees.qasim.mq@gmail.com>
2024-10-17 23:35:45 +0300

    add socket.io server

---
+++ server.js
@@ -0,0 +1,22 @@
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+
+const PORT = process.env.PORT === undefined ? 3000 : process.env.PORT;
+
+const httpServer = createServer();
+
+const io = new Server(httpServer, {
+    cors: {
+        origin: "http://localhost:" + PORT
+      }
+});
+
+io.on('connection', (socket) => {
+    socket.send('Hello World');
+
+    socket.on('message', () => {
+        console.log(message);
+    });
+});
+
+httpServer.listen(PORT);
